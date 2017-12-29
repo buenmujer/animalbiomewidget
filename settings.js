73e85602529d3e63edfb9d29dcd5f9dbd8a38a96
sandorolando <Sandor@MacBook-Air-3.local>
2017-12-29 15:11:21 -0800

    On

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // Change the url to the domain of your app
-exports.url = 'https://newsletteropin.heroku.com';
+exports.url = 'https://mailmodal.herokuapp.com';
 
 exports.senderEmail = "rolando@animalbiome.com";
 exports.senderName = "AnimalBiome";

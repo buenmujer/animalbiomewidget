06f872f6b1cecd590b8ac41fe054783cf1376bac
robo <rolando.sandor@gmail.com>
2018-02-13 10:59:15 -0800

    Lost forward slash.

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // Change the url to the domain of your app
-exports.url = 'https://imail.herokuapp.com/';
+exports.url = 'https://imail.herokuapp.com';
 
 exports.senderEmail = "rolando@animalbiome.com";
 exports.senderName = "Rolando Sandor";
